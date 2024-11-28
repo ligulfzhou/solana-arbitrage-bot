de5ccd55871177fd462bfd1364bb761c95033b84
ligulfzhou <ligulfzhou53@gmail.com>
2024-11-28 14:58:02 +0800

    add jup-agg

--- jup-agg/src/main.rs
+++ jup-agg/src/main.rs
@@ -1,6 +1,18 @@
+use serde::{Deserialize, Serialize};
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const JUP_V6_API: &str = "https://public.jupiterapi.com";
 const QUICKNODE_MAINNET: &str = "https://broken-delicate-diamond.solana-mainnet.quiknode.pro/50e19bfb7dfe66ceaafbc998ebfeda75f442d096";
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QuotaParam {
+    pub input_mint: String,
+    pub output_mint: String,
+    pub amount: u64
+}
+
 
 fn main() {
     println!("Hello, world!");
