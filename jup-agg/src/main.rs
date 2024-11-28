c36405cbcb86380a099deff8190be24a308b9acc
ligulfzhou <ligulfzhou53@gmail.com>
2024-11-28 14:28:35 +0800

    .gitignore ignore target

---
+++ jup-agg/src/main.rs
@@ -0,0 +1,7 @@
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const JUP_V6_API: &str = "https://public.jupiterapi.com";
+const QUICKNODE_MAINNET: &str = "https://broken-delicate-diamond.solana-mainnet.quiknode.pro/50e19bfb7dfe66ceaafbc998ebfeda75f442d096";
+
+fn main() {
+    println!("Hello, world!");
+}
