e37986e25245d175fca8e69e60903c31dd68a049
ligulfzhou <ligulfzhou53@gmail.com>
2024-12-01 12:42:09 +0800

    tmp

--- jup-agg/src/main.rs
+++ jup-agg/src/main.rs
@@ -5,20 +5,16 @@ mod mints;
 
 use crate::config::Config;
 use std::sync::Arc;
-// use crate::jup_api::{get_quote, get_swap_ixs};
 use crate::keypair_generator::KeypairGenerator;
-use anyhow::bail;
 use jupiter_swap_api_client::quote::QuoteRequest;
 use jupiter_swap_api_client::swap::SwapRequest;
-use jupiter_swap_api_client::transaction_config::TransactionConfig;
 use jupiter_swap_api_client::JupiterSwapApiClient;
 use serde::{Deserialize, Serialize};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_program::native_token::LAMPORTS_PER_SOL;
 use solana_program::pubkey::Pubkey;
 use solana_sdk::pubkey;
-use solana_sdk::signature::{Keypair, NullSigner, Signer};
-use solana_sdk::transaction::VersionedTransaction;
+use solana_sdk::signature::{Keypair, Signer};
 use std::time::Duration;
 
 pub const JUP_V6_API: &str = "https://public.jupiterapi.com";
@@ -30,30 +26,6 @@ const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
 const LUCE: Pubkey = pubkey!("CBdCxKo9QavR9hfShgpEBG3zekorAeD7W1jfq2o3pump");
 const ANTI: Pubkey = pubkey!("HB8KrN7Bb3iLWUPsozp67kS4gxtbA4W5QJX4wKPvpump");
 const MOODENG: Pubkey = pubkey!("ED5nyyWEzpPPiWimP8vYm7sD7TD3LAt3Q3gRTWHzPJBY");
-
-async fn sol_to_sol() -> anyhow::Result<()> {
-    // let initial_amount = LAMPORTS_PER_SOL;
-    // let mut quote0_res = get_quote(W_SOL_MINT, W_SOL_MINT, initial_amount).await?;
-    // dbg!(&quote0_res);
-    //
-    // let final_output_amount = quote0_res.out_amount.clone().parse::<u64>()?;
-    // if final_output_amount <= initial_amount {
-    //     let msg = format!(
-    //         "没有套利机会: 会亏损: {:?} Sol",
-    //         (initial_amount - final_output_amount) as f64 / LAMPORTS_PER_SOL as f64
-    //     );
-    //
-    //     println!("{}", msg);
-    //     bail!(msg);
-    // }
-    //
-    // println!(
-    //     "有的赚, 赚{:?}",
-    //     (final_output_amount - initial_amount) as f64 / LAMPORTS_PER_SOL as f64
-    // );
-
-    Ok(())
-}
 
 async fn arbitrage(
     payer: &Keypair,
